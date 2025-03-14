8dff0a5d855c6d5b13723f0eb6fe30cbea0ee24b
Alfeu Xerinda <alfeuxirinda@gmail.com>
2025-03-14 17:31:10 +0200

    rem dotenv

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,19 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
-import 'dotenv/config';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "consolelog-5e9b0.firebaseapp.com",
   projectId: "consolelog-5e9b0",
   storageBucket: "consolelog-5e9b0.firebasestorage.app",
   messagingSenderId: "231537152934",
   appId: "1:231537152934:web:632f693e90ee6e67bc3567",
   measurementId: "G-NXMJ9WLVVW",
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
