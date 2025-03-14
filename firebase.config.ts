b7d870074368bf6e2f5e495b8c4d619480874ace
Alfeu Xerinda <alfeuxirinda@gmail.com>
2025-03-14 02:24:18 +0200

    Saving work before rewriting history

---
+++ firebase.config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "consolelog-cc937.firebaseapp.com",
+  projectId: "consolelog-cc937",
+  storageBucket: "consolelog-cc937.firebasestorage.app",
+  messagingSenderId: "24175104534",
+  appId: "1:24175104534:web:58bc8c2b63e9ef1db3db37",
+  measurementId: "G-XKXT02TE9K",
+  databaseURL: "https://consolelog-cc937-default-rtdb.firebaseio.com",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
