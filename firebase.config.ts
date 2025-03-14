be216e842fef3af5bfb0372afc8e3e08973ce5a4
Alfeu Xerinda <alfeuxirinda@gmail.com>
2025-03-14 16:08:42 +0200

    add env file

--- firebase.config.ts
+++ firebase.config.ts
@@ -6,14 +6,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnj2cd8ii0qDFbfb4o4AKqK1qmq8z-olY",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "consolelog-5e9b0.firebaseapp.com",
   projectId: "consolelog-5e9b0",
   storageBucket: "consolelog-5e9b0.firebasestorage.app",
   messagingSenderId: "231537152934",
   appId: "1:231537152934:web:632f693e90ee6e67bc3567",
   measurementId: "G-NXMJ9WLVVW",
-  databaseURL: "https://consolelog-5e9b0-default-rtdb.firebaseio.com",
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
